0632b3fda61348ea0bd65d26131699152ae1df25
Mallory Robertson <malloryworks@gmail.com>
2020-01-21 20:17:43 -0800

    Added faker, started comments

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 // Create a react component
 const App = () => {
-	
-	const buttonText = "Click me!";
-	const labelText = "Enter Name: ";
-
 	return (
-	<div>
-  		<label className="label" htmlFor="name">{labelText}</label>
-  		<input id="name" type="text" />
-  		<button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText}</button>
-	</div>
+		<div className="ui container comments">
+			<div className="comment">
+				<a href="/" className="avatar">
+					<img src={faker.image.avatar()} alt="avatar"/>
+				</a>
+				<div className="content">
+					<a href="/" className="author">
+						Sam
+					</a>
+					<div className="metadata">
+						<span className="date">Today at 6:00PM</span>
+					</div>
+					<div className="text">Nice blog post!</div>
+				</div>
+			</div>
+		</div>
 	);
 };
+
+
 
 // Take the react component and show it on the screen
 ReactDOM.render(
