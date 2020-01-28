6592bd267167bc27429fd8adbe60080959c2db26
Mallory Robertson <malloryworks@gmail.com>
2020-01-27 19:43:31 -0800

    Added more props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -5,16 +5,16 @@ const CommentDetail = (props) => {
 	return (
 		<div className="comment">
 			<a href="/" className="avatar">
-				<img src={faker.image.avatar()} alt="avatar"/>
+				<img src={props.avatar} alt="avatar"/>
 			</a>
 			<div className="content">
 				<a href="/" className="author">
 					{props.author}
 				</a>
 				<div className="metadata">
-					<span className="date">Today at 6:00PM</span>
+					<span className="date">{props.timeAgo}</span>
 				</div>
-				<div className="text">Nice blog post!</div>
+				<div className="text">{props.commentText}</div>
 			</div>
 		</div>
 	);
--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import CommentDetail from './CommentDetail'; // ./ says look in the same folder
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<CommentDetail author="Sam" />
-			<CommentDetail author="Alex" />
-			<CommentDetail author="Jane"/>
-			<CommentDetail author="Mallory"/>
-			<CommentDetail author="Paul"/>
+			<CommentDetail author="Sam" timeAgo="Today at 4:45PM" commentText="Nice blog post!" avatar={faker.image.avatar()}/>
+			<CommentDetail author="Alex" timeAgo="Today at 2:00PM" commentText="I totally agree." avatar={faker.image.avatar()}/>
+			<CommentDetail author="Jane" timeAgo="Yesterday at 8:03AM" commentText="Samesies!" avatar={faker.image.avatar()}/>
+			<CommentDetail author="Mallory" timeAgo="Yesterday at 7:45AM" commentText="I can't believe it." avatar={faker.image.avatar()} />
+			<CommentDetail author="Paul" timeAgo="Yesterday at 2:23AM" commentText="Love this <333" avatar={faker.image.avatar()}/>
 		</div>
 	);
 };
